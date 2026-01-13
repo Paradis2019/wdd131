9a06e6b4fead86cd23c85a3892c639077a0537c1
Paradis2019 <popol.elonda@byupw.edu>
2026-01-13 03:52:58 -0700

    uodate

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,12 +1,18 @@
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
+menuButton.textContent = "☰";
+
 menuButton.addEventListener("click", () => {
   navigation.classList.toggle("show");
-  menuButton.textContent = navigation.classList.contains("show") ? "X" : "☰";
+
+  menuButton.textContent = navigation.classList.contains("show")
+    ? "X"
+    : "☰";
 });
 
-document.querySelector("#year").textContent = new Date().getFullYear();
+const yearSpan = document.querySelector("#year");
+yearSpan.textContent = new Date().getFullYear();
 
-document.querySelector("#lastModified").textContent =
-  `Last Modified: ${document.lastModified}`;
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
