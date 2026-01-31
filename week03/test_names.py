d0f8d485332b3c1f6f22816c44d431958b5cd0ed
Paradis2019 <popol.elonda@byupw.edu>
2026-01-30 19:10:10 -0700

    update

--- week03/test_names.py
+++ week03/test_names.py
@@ -1,35 +1,36 @@
 import pytest
 from names import make_full_name, extract_family_name, extract_given_name
 
-
 def test_make_full_name():
     # short names
-    assert make_full_name("John", "Smith") == "Smith; John"
+    assert make_full_name("Sally", "Brown")=="Sally", "Brown"
 
     # long names
-    assert make_full_name("Elizabeth", "Washington") == "Washington; Elizabeth"
+    assert make_full_name("Elizabeth", "Washington")=="Washington; Elizabeth"
 
     # hyphenated family name
-    assert make_full_name("Ava", "Smith-Jones") == "Smith-Jones; Ava"
+    assert make_full_name("Ava", "Smith-Jones")=="Smith-Jones; Ava"
 
 
 def test_extract_family_name():
     # short name
-    assert extract_family_name("Smith; John") == "Smith"
+    assert extract_family_name("Brown; Sally")=="Brown"
 
     # long name
-    assert extract_family_name("Washington; Elizabeth") == "Washington"
+    assert extract_family_name("Washington; Elizabeth")=="Washington"
 
     # hyphenated family name
-    assert extract_family_name("Smith-Jones; Ava") == "Smith-Jones"
+    assert extract_family_name("Smith-Jones; Ava")=="Smith-Jones"
 
 
 def test_extract_given_name():
     # short name
-    assert extract_given_name("Smith; John") == "John"
+    assert extract_given_name("Brown; Sally")=="Sally"
 
     # long name
-    assert extract_given_name("Washington; Elizabeth") == "Elizabeth"
+    assert extract_given_name("Washington; Elizabeth")=="Elizabeth"
 
     # hyphenated family name
-    assert extract_given_name("Smith-Jones; Ava") == "Ava"
+    assert extract_given_name("Smith-Jones; Ava")=="Ava"
+
+    pytest.main(["-v", "--tb=line", "-rN", __file__])
